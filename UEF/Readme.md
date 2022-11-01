f51fa4e68f4bcab5b686fc3ef681a9c477c99840
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-01 17:17:48 -0300

    Criando projeto e atualizando o Readme local

---
+++ UEF/Readme.md
@@ -0,0 +1,16 @@
+## Ultimate Emoji Fight
+
+* Relacionamento enre Classes Aula07
+
+Estou estudando sobre relacionamento entre classes no curso
+de POO do Gustavo Guanabara
+link: https://youtu.be/GLHbxDU9iBA
+
+* Sobre o projeto
+
+Criar uma classe com nome Fighter(Lutador)
+Com seus respectivos atributos e metodos. Adicionei os metodos especiais
+Getters e Setters. 
+
+
+
---
+++ UEF/src/Fighter.java
@@ -0,0 +1,107 @@
+public class Fighter {
+    //atributes
+    private String name;
+    private String nationality;
+    private int age;
+    private float height;
+    private float weight;
+    private String category;
+    private int wins;
+    private int defeats;
+    private int draws;
+
+    // Methods
+
+    public void shows() {
+
+    }
+
+    public void status() {
+
+    }
+
+    public void winFight() {
+
+    }
+
+    public void loseFight() {
+
+    }
+
+    public void drawFight() {
+
+    }
+
+    // Special Methods
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public float getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public float getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public String getCategory() {
+        return this.category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getWins() {
+        return this.wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getDefeats() {
+        return this.defeats;
+    }
+
+    public void setDefeats(int defeats) {
+        this.defeats = defeats;
+    }
+
+    public int getDraws() {
+        return this.draws;
+    }
+
+    public void setDraws(int draws) {
+        this.draws = draws;
+    }
+}
