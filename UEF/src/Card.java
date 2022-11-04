64b870e25f3038f36f63ee468ad8222b3c652b5e
Rodrigo Felix <rodrigofelixferreira@hotmail.com>
2022-11-03 22:37:04 -0300

    Adicionando Classe (CARD) para treinar interação entre as classes

---
+++ UEF/src/Card.java
@@ -0,0 +1,52 @@
+public class Card {
+    //  Attributes
+    private String challenged;
+    private String challenger;
+    private int rounds;
+    private int approved;
+
+    // Methods
+
+    public void scheduleFight() {
+
+    }
+
+    public void fight() {
+
+    }
+
+    // Methods
+
+
+    public String getChallenged() {
+        return challenged;
+    }
+
+    public void setChallenged(String challenged) {
+        this.challenged = challenged;
+    }
+
+    public String getChallenger() {
+        return challenger;
+    }
+
+    public void setChallenger(String challenger) {
+        this.challenger = challenger;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
+
+    public int getApproved() {
+        return approved;
+    }
+
+    public void setApproved(int approved) {
+        this.approved = approved;
+    }
+}
